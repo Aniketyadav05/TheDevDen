637d78812e20b90cb15453c7a3a7c9313aa11389
Aniket Yadav <yadavaniket7611@gmail.com>
2025-03-24 21:45:00 +0530

    Home updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -73,7 +73,7 @@ const Home = () => {
   <div className="flex-1 flex flex-col items-center justify-center">
     <SplitText
       text="WANT TO POST BLOGS AND SEE OTHER BLOGS"
-      className="text-8xl font-semibold text-center text-gray-800  "
+      className="text-8xl font-semibold text-center text-gray-800  pt-4"
       delay={100}
       animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
       animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
